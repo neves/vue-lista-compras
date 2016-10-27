f122994d2dda54900ffe8098f6cf68ea58e2885d
Marcos Neves <marcos.neves@gmail.com>
2016-10-26 23:43:27 -0200

    Passo 06: Exibir a lista de produtos dinamicamente

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    titulo: 'Lista de Compras',
+    tituloMaxlength: 16,
+    produtos: [
+      {nome: 'Arroz'},
+      {nome: 'Feijão'},
+    ],
+  },
+})
