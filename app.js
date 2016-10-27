d43f7ee34fd318cdc57db1d3945a126a12d290f2
Marcos Neves <marcos.neves@gmail.com>
2016-10-27 03:12:02 -0200

    Passo 10.3: Diretiva para focar o input ao editar

---
+++ app.js
@@ -0,0 +1,46 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    titulo: 'Lista de Compras',
+    tituloMaxlength: 16,
+    produtos: [
+      {nome: 'Arroz', comprado: false},
+      {nome: 'Feijão', comprado: true},
+    ],
+    novoProduto: '',
+    produtoSelecionado: null,
+  },
+
+  methods: {
+    add () {
+      var novo = {
+        nome: this.novoProduto,
+        comprado: false
+      }
+      this.produtos.push(novo)
+      this.novoProduto = ''
+    },
+
+    excluir (i) {
+      if (confirm('Tem certeza?')) {
+        this.produtos.splice(i, 1)
+      }
+    },
+
+    editando (produto) {
+      return produto === this.produtoSelecionado
+    },
+
+    selecionar (produto) {
+      this.produtoSelecionado = produto
+    },
+  },
+
+  directives: {
+    autofocus: {
+      inserted (el) {
+        el.focus()
+      },
+    },
+  },
+})
