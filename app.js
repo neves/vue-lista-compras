e2caecc4cefecc99fc50f274c3389bea167b02e7
Marcos Neves <marcos.neves@gmail.com>
2016-10-27 10:16:50 -0200

    Passo 12: Mover o codigo de formatação do título do template para uma "computed property"

---
+++ app.js
@@ -0,0 +1,58 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    titulo: 'Lista de Compras',
+    tituloMaxlength: 16,
+    produtos: [
+      {nome: 'Arroz', comprado: false},
+      {nome: 'Feijão', comprado: true},
+    ],
+    novoProduto: '',
+    produtoSelecionado: null,
+  },
+
+  computed: {
+    tituloFormatado () {
+      return this.titulo.toUpperCase().substr(0, this.tituloMaxlength)
+    },
+  },
+
+  methods: {
+    add () {
+      var novo = {
+        nome: this.novoProduto,
+        comprado: false
+      }
+      this.produtos.push(novo)
+      this.novoProduto = ''
+    },
+
+    excluir (i) {
+      if (confirm('Tem certeza?')) {
+        this.produtos.splice(i, 1)
+      }
+    },
+
+    editando (produto) {
+      return produto === this.produtoSelecionado
+    },
+
+    selecionar (produto) {
+      this.produtoSelecionado = produto
+    },
+  },
+
+  directives: {
+    autofocus: {
+      inserted (el) {
+        el.focus()
+      },
+    },
+  },
+
+  filters: {
+    pluralize (count, palavra) {
+      return count == 1 ? `${count} ${palavra}` : `${count} ${palavra}s`
+    },
+  },
+})
